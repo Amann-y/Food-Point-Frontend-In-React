2397e50c1e5a2c02c82dd9b8e34c1023acfb5034
Amann-y <yadavaman413@gmail.com>
2024-06-19 14:50:22 +0530

    update 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import {useNavigate} from "react-router-dom"
 
 const NotFound = () => {
+  const navigate = useNavigate()
   return (
     <Container>
       <Row>
-        <Col>
+        <Col className="justify-content-center align-items-center py-3 d-flex flex-column">
           <h3 className="text-danger">404 page not found</h3>
           <p>We are sorry but the page you are looking for does not exist.</p>
-          <button className="btn btn-outline-light my-2">Back To Home Page</button>
+          <button className="btn btn-outline-dark my-2" onClick={()=>navigate("/")}>Back To Home Page</button>
         </Col>
       </Row>
     </Container>
